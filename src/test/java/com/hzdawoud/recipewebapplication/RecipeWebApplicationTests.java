c6f8e420c9d791d0193db3ecd00ea1230f69b615
hzdawoud <hzdawoud@gmail.com>
2021-05-09 09:48:50 +0300

    initial project

---
+++ src/test/java/com/hzdawoud/recipewebapplication/RecipeWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hzdawoud.recipewebapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
