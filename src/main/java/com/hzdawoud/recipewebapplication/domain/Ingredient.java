2b1c7d9592e2b1f03f1e21783fc57e61890ffff8
hzdawoud <hzdawoud@gmail.com>
2021-05-16 07:21:17 +0300

    fixing lombok hashcode error

--- src/main/java/com/hzdawoud/recipewebapplication/domain/Ingredient.java
+++ src/main/java/com/hzdawoud/recipewebapplication/domain/Ingredient.java
@@ -6,6 +6,7 @@ import javax.persistence.*;
 import java.math.BigDecimal;
 
 @Data
+@EqualsAndHashCode(exclude = {"uom"})
 @Entity
 public class Ingredient {
 
--- src/main/java/com/hzdawoud/recipewebapplication/domain/Recipe.java
+++ src/main/java/com/hzdawoud/recipewebapplication/domain/Recipe.java
@@ -1,12 +1,14 @@
 package com.hzdawoud.recipewebapplication.domain;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
 @Data
+@EqualsAndHashCode(exclude = {"ingredients", "note", "categories"})
 @Entity
 public class Recipe {
 
