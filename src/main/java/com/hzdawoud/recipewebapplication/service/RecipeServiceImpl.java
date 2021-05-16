b4df0453481edfc25f75bc7af824217743486c24
hzdawoud <hzdawoud@gmail.com>
2021-05-16 07:07:01 +0300

    adding lombok refactoring plugin

--- src/main/java/com/hzdawoud/recipewebapplication/service/RecipeServiceImpl.java
+++ src/main/java/com/hzdawoud/recipewebapplication/service/RecipeServiceImpl.java
@@ -2,12 +2,14 @@ package com.hzdawoud.recipewebapplication.service;
 
 import com.hzdawoud.recipewebapplication.domain.Recipe;
 import com.hzdawoud.recipewebapplication.repository.RecipeRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.Set;
 
 @Service
+@Slf4j
 public class RecipeServiceImpl implements RecipeService {
     private final RecipeRepository recipeRepository;
 
@@ -17,6 +19,7 @@ public class RecipeServiceImpl implements RecipeService {
 
     @Override
     public Set<Recipe> getRecipes() {
+        log.debug("In Recipe Service...");
         Set<Recipe> recipeSet = new HashSet<>();
         recipeRepository.findAll().iterator().forEachRemaining(recipeSet::add);
         return recipeSet;
